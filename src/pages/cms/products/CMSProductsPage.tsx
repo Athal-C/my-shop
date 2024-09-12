ca2d92fd7a6387ddce597ff84c335d0c0f967945
Athal-C <ccristian02@gmail.com>
2024-09-12 16:42:48 +0200

    chapter 8 completion

--- src/pages/cms/products/CMSProductsPage.tsx
+++ src/pages/cms/products/CMSProductsPage.tsx
@@ -1,10 +1,31 @@
+import { useProductService } from "@/services/products";
+
 export function CMSProductsPage() {
+  const { state, actions } = useProductService();
+
+  async function getProductsHandler() {
+    actions.getProducts();
+  }
+
   return (
     <div>
       <h1 className='title'>CMS</h1>
       <p className='flex font-bold text-xl justify-center text-center p-3'>
         Pagina Prodotti
       </p>
+      <hr className='my-8' />
+
+      {state.pending && <div>loading...</div>}
+      {state.error && (
+        <div className='flex font-bold text-2xl justify-center text-center p-3'>
+          An error has occurred
+        </div>
+      )}
+
+      <button className='btn primary' onClick={getProductsHandler}>
+        GET
+      </button>
+      <pre>{JSON.stringify(state, null, 2)}</pre>
     </div>
   );
 }
---
+++ src/services/products/index.ts
@@ -0,0 +1,1 @@
+export { useProductService } from "./useProductsService";
